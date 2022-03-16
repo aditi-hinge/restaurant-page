a8e911a65ba0451c980ea6556f50ad563012f0c1
aditi-hinge <aditi.hinge@gmail.com>
2022-03-16 21:11:32 +0900

    add elements through JS

--- src/pageload.js
+++ src/pageload.js
@@ -1,10 +1,22 @@
+import Cat from './cat.jpeg';
+
+let container = document.getElementById("container");
+
 const pageLoad=()=>{
     //creating heading
     const heading = document.createElement("h2");
+    container.appendChild(heading);
+    heading.textContent = "Welcome to Freddy FazzBear's Pizzeria!";
+    
+    //add image
+    const myBackground = new Image();
+    myBackground.src = Cat;
+    container.appendChild(myBackground);
 
     //writing the heading 
-    heading.textContent = "Welcome to Freddy FazzBear's Pizzeria!";
-    return heading;
+    const review = document.createElement("div");
+    review.textContent = "meow meow meow";
+    container.appendChild(review);
 }
 
 //append it
