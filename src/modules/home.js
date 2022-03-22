0dc7f71512d33e7970d57060f6602518e68f61f1
aditi-hinge <aditi.hinge@gmail.com>
2022-03-22 22:41:01 +0900

    moved modules to folder

---
+++ src/modules/home.js
@@ -0,0 +1,61 @@
+// import './style.css';
+
+const container = document.getElementById("container");
+
+// home tab
+const homeContent = () => {
+    const homeDiv = document.createElement("div");
+    homeDiv.setAttribute("id","homeContent");
+    container.appendChild(homeDiv);
+
+    // create
+    const welcome = document.createElement("div");
+    welcome.textContent = "Welcome to Freddy Fazzbear's Pizzaria";
+
+    // set attribute
+    welcome.setAttribute("id", "welcome");
+
+    //append
+    homeDiv.appendChild(welcome);
+    
+    return container;
+}
+
+// buttons for tabs
+const tabButtons = () => {
+
+    // create tab div
+    const tab = document.createElement("div");
+    tab.setAttribute("id","tab");
+    container.appendChild(tab);
+
+    // create
+    const homeButton = document.createElement("button");
+    const menuButton = document.createElement("button");
+    const locationButton = document.createElement("button");
+
+    // set IDs
+    homeButton.setAttribute("id", "homeBtn");
+    menuButton.setAttribute("id", "menuBtn");
+    locationButton.setAttribute("id", "locationBtn");
+
+    // set class names
+    homeButton.setAttribute("class", "navBtn");
+    menuButton.setAttribute("class", "navBtn");
+    locationButton.setAttribute("class", "navBtn");
+
+    // name
+    homeButton.textContent = "HOME";
+    menuButton.textContent = "MENU";
+    locationButton.textContent = "LOCATION";
+
+    // append
+    tab.appendChild(homeButton);
+    tab.appendChild(menuButton);
+    tab.appendChild(locationButton);
+
+    // return
+    return container
+}
+
+export { homeContent, tabButtons };
---
+++ src/modules/location.js
@@ -0,0 +1,31 @@
+import '../style.css';
+
+const container = document.getElementById("container");
+
+const locationContent=()=>{
+    console.log("location here!");
+    const locationBox = document.createElement("div");
+    locationBox.setAttribute("id", "locationBox");
+    container.appendChild(locationBox);
+
+    // contact number
+    const contact = document.createElement("div");
+    contact.setAttribute("class", "contact");
+    contact.textContent = "555-555-555"
+    locationBox.appendChild(contact);
+
+    // address
+    const address = document.createElement("div");
+    address.setAttribute("class", "contact");
+    address.textContent = "1600 Pennsylvania Avenue NW, Washington, DC 20500, United States"
+    locationBox.appendChild(address);
+
+    // map
+    const map = document.createElement("div");
+    map.setAttribute("id", "map");
+    locationBox.appendChild(map);
+
+    return container;
+}
+
+export { locationContent };
